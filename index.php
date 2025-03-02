217c08895fa666661fdd0d1add99bb9c6e791f43
Andrew Skow <skovorodnikov11@gmail.com>
2025-03-02 14:28:10 +0300

    finished_start_php

--- index.php
+++ index.php
@@ -32,6 +32,35 @@ $sum = function ($a, $b) {
 // вызывать фукнцию можно по той же переменной как обычную функцию 
 // переменная, по которой вызывется функция является замыканием
 
+$users = [
+  ['name' => 'Alena',
+  'age' => 25,
+  'city' => 'Moscow'
+],
+  ['name' => 'Vladimir',
+  'age' => 30,
+  'city' => 'Saint Petersburg'
+],
+  ['name' => 'Olga',
+  'age' => 28,
+  'city' => 'London']
+];
+
+// usort() - встроенная функция для сортировки
+// usort  в качестве первого аргумента принимает массив, 
+// в качестве второго аргумента принимает - callback 
+
+function print_array ($array){
+  echo "<pre>";
+  print_r($array);
+  echo "</pre>";
+}
+
+$words = ['something', 'else', 'nothing', 'apple', 'orange'];
+$filtredArray = array_filter($words, function ($word) {
+  return strlen($word) > 5; // остаются слова только длиной больше 5 символов
+});
+
 ?>
 
 
@@ -45,10 +74,21 @@ $sum = function ($a, $b) {
   <style>
     body {
       font-family: sans-serif;
+      box-sizing: border-box;
     }
 
-    h1 {
+    h1, h2 {
       text-align: center;
+    }
+
+    .theory {
+      margin-top: 15px;
+      margin-bottom: 15px;
+      padding: 10px;
+      border-radius: 15px;
+      background-color: lightgreen;
+      box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+      text-align: justify;
     }
 
     .php_result {
@@ -58,7 +98,7 @@ $sum = function ($a, $b) {
       color: green;
       background-color: black;
       padding: 10px;
-      border-radius: 5px;
+      border-radius: 15px;
     }
 
     div.php_result>h6 {
@@ -139,7 +179,7 @@ $sum = function ($a, $b) {
     </div>
     </div>
 
-
+    <div class="theory">
     <h2>Анонимные функции</h2>
     <p>
       Анонимыне функция - замыкания - особый тип функций, который может 
@@ -152,7 +192,10 @@ $sum = function ($a, $b) {
       Анонимные функции могут быть также полезны, где ожидается колбек. 
       Колбек - передача функции в качестве аргумента в другую функцию или метод. 
       В качестве аргумента можно также переадавать анонимную функцию. 
+      С помощью ключевого слова use($variable) можно предать глобальную переменную 
+      в анонимную функцию . 
     </p>
+    </div>
 
     <div class="container_grid">
       <div class="php_result">
@@ -161,10 +204,39 @@ $sum = function ($a, $b) {
         $sum = $sum(5, 10);
         echo $sum;
        ?>
+        </div>
+
+<div class="php_result">
+        <h6>Использование callback функции</h6>
+        <?php
+        echo "<h6>Начальный массив</h6>";
+        print_array($users);
+        usort($users, function ($a, $b)  {
+          // $a and $b - два элемента массива users, которые нужно сравнить 
+          // внутри анонимной функции записывается логика сравнения массивов 
+          return $a['age'] - $b['age']; // сравниваем возрасты, если они равны, то сравниваем имена
+          // если результат будет ортцательный, то а будет располагаться перед б 
+          // если равен нулю, то порядок элементов не изменится 
+        });
+        echo "<h6>Отсортированный массив</h6>";
+        print_array($users);
+       ?> 
+
+        </div>
+<div class="php_result">
+        <h6>Использование array_filter</h6>
+        <?php
+        echo "<h6>Начальный массив слов</h6>";
+        print_array($words);
+        echo "<h6>Фильтрованный массив</h6>";
+        print_array($filtredArray);
+        ?> 
+        </div>
+
       </div>
 
     </div>
-  </>
+  
 </body>
 
 </html>
